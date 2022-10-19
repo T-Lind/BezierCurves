588ef0e9867786dfec0fbb96b7a345726b07ac4b
T-Lind <tiernanxklindauer@gmail.com>
2022-10-19 18:04:09 -0500

    Initial commit

---
+++ Bezier.py
@@ -0,0 +1,215 @@
+import math
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "X: " + str(self.x) + " Y: " + str(self.y) + "\n"
+
+
+class Bezier:
+    def __init__(self, control_values, resolution=0.001):
+        # Storage of bezier curve points
+        self.point_storage = []
+
+        # Store the derivative of the entire line
+        self.velocity = []
+        self.total_derivative = []
+        self.angles = []
+
+        # Store the derivative of the x and y components, distance from (0,0) is total derivative
+        self.derivative_component_storage = []
+
+        # The offsets from the center line on the left and right and their angles and velocities
+        self.left_offset_storage = []
+        self.right_offset_storage = []
+
+        self.left_offset_velocity = []
+        self.left_angles = []
+
+        self.right_offset_velocity = []
+        self.right_angles = []
+
+        # List of distances for each increase in resolution time
+        self.distances = []
+
+        # Info for equally spaced bezier curve
+        self.equally_spaced_times = []
+        self.equally_spaced_points = []
+        self.equally_spaced_velocity = []
+
+        # Provided control values
+        self.control_values = control_values
+
+        # The resolution of the bezier curve
+        self.resolution = resolution
+
+        # A list of times one less than 1/resolution long
+        self.time_list = []
+        for current_time in range(0, int(1 / self.resolution) - 1, 1):
+            self.time_list.append(current_time * self.resolution)
+
+        # Create the bezier curve
+        for current_time in range(0, int(1 / self.resolution), 1):
+            current_point = Bezier.recursive_lerp(self.control_values, current_time * self.resolution)
+            self.point_storage.append(current_point)
+
+    @staticmethod
+    def lerp(point0, point1, t):
+        x = (1 - t) * point0.x + t * point1.x
+        y = (1 - t) * point0.y + t * point1.y
+        return Point(x, y)
+
+    @staticmethod
+    def recursive_lerp(points, time):
+        if len(points) == 2:
+            return Bezier.lerp(points[0], points[1], time)
+
+        index = 0
+        list_of_lerps = []
+        while index < len(points) - 1:
+            point1 = Bezier.lerp(points[index], points[index + 1], time)
+            list_of_lerps.append(point1)
+            index += 1
+
+        return Bezier.recursive_lerp(list_of_lerps, time)
+
+    @staticmethod
+    def get_x(points):
+        x_list = []
+        for point in points:
+            x_list.append(point.x)
+        return x_list
+
+    @staticmethod
+    def get_y(points):
+        y_list = []
+        for point in points:
+            y_list.append(point.y)
+        return y_list
+
+    @staticmethod
+    def norm(nums):
+        normalized_list = []
+
+        maximum = max(nums)
+        minimum = min(nums)
+
+        for item in nums:
+            normalized_list.append((item - minimum) / (maximum - minimum))
+        return normalized_list
+
+    # Returns index of x in arr if present, else -1
+    @staticmethod
+    def binary_search(arr, low, high, x):
+        # Check base case
+        if high >= low:
+
+            mid = (high + low) // 2
+
+            # If element is present at the middle itself
+            if arr[mid-1] < x < arr[mid+1]:
+                return mid
+
+            # If element is smaller than mid, then it can only
+            # be present in left subarray
+            elif arr[mid] > x:
+                return Bezier.binary_search(arr, low, mid - 1, x)
+
+            # Else the element can only be present in right subarray
+            else:
+                return Bezier.binary_search(arr, mid + 1, high, x)
+
+        else:
+            # Element is not present in the array
+            return -1
+
+    # Calculate the derivative of the bezier in x and y, the velocity, and the total (or raw) derivative of the curve
+    def calculate_bezier_derivative(self):
+        for i in range(0, len(self.point_storage) - 1):
+            # Calculate the x and y velocity components (first deriv)
+            derivative_x = (self.point_storage[i + 1].x - self.point_storage[i].x) / self.resolution
+            derivative_y = (self.point_storage[i + 1].y - self.point_storage[i].y) / self.resolution
+            self.derivative_component_storage.append(Point(derivative_x, derivative_y))
+
+            # Calculate the total derivative and angle of the bezier curve (magnitude of (derivative_x, derivative_y)
+            velocity = math.sqrt(derivative_x ** 2 + derivative_y ** 2)
+            self.velocity.append(velocity)
+
+            # Calculate the total derivative
+            self.total_derivative.append((derivative_y * self.resolution) / (derivative_x * self.resolution))
+
+            # Calculate the current angle
+            self.angles.append(math.degrees(math.atan2((derivative_y * self.resolution),
+                                                       (derivative_x * self.resolution))))
+
+    # Create the distance table
+    def calculate_distances(self):
+        cumulative_distance = 0
+        for i in range(0, len(self.point_storage) - 1):
+            distance = math.sqrt((self.point_storage[i + 1].x - self.point_storage[i].x) ** 2 + (
+                        self.point_storage[i + 1].y - self.point_storage[i].y) ** 2)
+            cumulative_distance += distance
+            self.distances.append(cumulative_distance)
+
+    def find_time_for_distance(self, distance):
+        index = self.binary_search(self.distances, 0, len(self.distances)-1, distance)
+        time = index * self.resolution
+        return time
+
+    def find_point_for_distance(self, distance):
+        index = self.binary_search(self.distances, 0, len(self.distances)-1, distance)
+        return self.point_storage[index]
+
+    def compose_equal_distance_bezier(self):
+        distance = 0
+        while distance < self.distances[-1]:
+            self.equally_spaced_times.append(self.find_time_for_distance(distance))
+            self.equally_spaced_points.append(self.find_point_for_distance(distance))
+            distance += 0.05
+
+    def calculate_equal_distance_velocity(self):
+        for i in range(0, len(self.equally_spaced_points) - 1):
+            # Calculate the x and y velocity components (first deriv)
+            derivative_x = (self.equally_spaced_points[i + 1].x - self.equally_spaced_points[i].x) / (self.equally_spaced_times[i + 1] - self.equally_spaced_times[i])
+            derivative_y = (self.equally_spaced_points[i + 1].y - self.equally_spaced_points[i].y) / (self.equally_spaced_times[i + 1] - self.equally_spaced_times[i])
+
+            # Calculate the total derivative and angle of the bezier curve (magnitude of (derivative_x, derivative_y)
+            velocity = math.sqrt(derivative_x ** 2 + derivative_y ** 2)
+            self.equally_spaced_velocity.append(velocity)
+
+    # Store the points for each offset into storage - TODO: Fix the flipping at 0 degrees
+    def calculate_offset(self, offset):
+        sign = 1
+        for i in range(0, len(self.total_derivative)):
+            normal = -1 / (self.total_derivative[i])
+
+            x0 = self.point_storage[i].x
+            y0 = self.point_storage[i].y
+
+            angle = math.atan(normal)
+
+            x1 = offset * math.cos(angle) + x0
+            y1 = offset * math.sin(angle) + y0
+
+            x2 = offset * -math.cos(angle) + x0
+            y2 = offset * -math.sin(angle) + y0
+
+            self.left_offset_storage.append(Point(x1, y1))
+            self.right_offset_storage.append(Point(x2, y2))
+
+    # Calculate the velocity and angle over time of the offsets
+    def calculate_offset_velocity_angles(self):
+        b_left = Bezier(self.control_values, self.resolution)
+        b_right = Bezier(self.control_values, self.resolution)
+
+        b_left.point_storage = self.left_offset_storage
+        b_right.point_storage = self.right_offset_storage
+
+        b_left.calculate_bezier_derivative()
+        b_right.calculate_bezier_derivative()
+
+        return b_left.velocity  # , b_left.angles, b_right.velocity, b_right.angles
---
+++ BezierTest.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+from Bezier import Bezier
+from Bezier import Point
+
+fig, axis = plt.subplots(2, 2)
+
+RESOLUTION = 0.1
+
+control_values = [Point(0, 0), Point(2,0.25), Point(0,1.5),Point(0,0.25)]
+
+
+b = Bezier(control_values, RESOLUTION)
+b.calculate_bezier_derivative()
+
+b.calculate_distances()
+
+
+axis[0][0].set_title("Bezier Curve with offsets")
+axis[0][0].scatter(b.get_x(b.point_storage), b.get_y(b.point_storage), alpha=0.1)
+
+# axis[0][0].scatter(b.get_x(b.left_offset_storage), b.get_y(b.left_offset_storage), alpha=0.01, color="RED")
+# axis[0][0].scatter(b.get_x(b.right_offset_storage), b.get_y(b.right_offset_storage), alpha=0.01, color="GREEN")
+
+axis[0][1].set_title("X and Y first derivative output")
+axis[0][1].plot(b.get_x(b.derivative_component_storage), b.get_y(b.derivative_component_storage))
+
+# axis[1][0].set_title("Angle in degrees of the bezier curve")
+# axis[1][0].scatter(b.time_list, b.angles, alpha=0.01)
+
+# axis[1][0].scatter(b.time_list, left_v, color="RED")
+# axis[1][0].scatter(b.time_list, right_v, color="GREEN")
+
+# axis[1][1].set_title("Non-normalized velocity")
+# axis[1][1].plot(b.time_list, b.velocity)
+#
+axis[1][0].set_title("Distance over time")
+axis[1][0].plot(b.time_list, b.distances)
+#
+# axis[2][1].set_title("Equal distance bezier")
+# axis[2][1].plot(b.get_x(b.equally_spaced_points), b.get_y(b.equally_spaced_points))
+#
+# axis[3][1].set_title("Equal distance bezier velocity")
+# axis[3][1].scatter(b.equally_spaced_times[0:-2], b.equally_spaced_velocity[1:])
+
+plt.show()
